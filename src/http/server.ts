8a0c94168f0dae43e208138ac5544ba0e04e0ab0
augustopolonio <augustodrp@gmail.com>
2024-02-14 14:28:42 -0300

    Basic config for testing fastify

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World! 🌍')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello NLW'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server is running 🚀 \nHello World! 🌍')
+})
